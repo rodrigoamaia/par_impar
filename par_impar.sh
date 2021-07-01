6ffa58ac372898addaaf8c849c90650677aac8d9
Rodrigo Maia <rodrigoamaia@gmail.com>
2021-07-01 05:58:40 -0300

    Adicionando Opção de escolha

--- par_impar.sh
+++ par_impar.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 clear
-echo ">> Digite um número"
-	read NUM
-echo
-	if [[ "${NUM}"%2 -eq 0 ]]
+echo ">> Digite seu nome"
+	read NOME
+	echo
+echo ">> Olá ${NOME}, deseja saber se um número é par ou impar[s/n]?"
+	read NOME
+	echo
+if [[ "${NOME}" == "n" ]]
 then
-	echo ">> O número digitado [${NUM}] é Par"
+	echo "** Obrigado, encerrando o sistema..."
 	echo
 else
-	echo ">> O número digitado [${NUM}] é Impar"
+	echo "--------------------------------------------"
+	echo ">> Digite um número"
+	read NUM
 	echo
+	if [[ "${NUM}"%2 -eq 0 ]]
+	then
+		echo ">> O número digitado [${NUM}] é Par"
+		echo
+	else
+		echo ">> O número digitado [${NUM}] é Impar"
+		echo
+	fi
 fi
